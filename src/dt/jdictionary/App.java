28ed16ae4cb8ae8c3c7c1c4240eb4d4bc2338749
Daniel <danieltjandra@gmail.com>
2024-07-08 22:04:18 -0700

    New version

--- src/dt/jdictionary/App.java
+++ src/dt/jdictionary/App.java
@@ -4,9 +4,10 @@ import dt.jdictionary.ui.UiConstants;
 import dt.jdictionary.ui.UiMain;
 import dt.jdictionary.util.Debug;
 
+// V1.3: 2024-07-08: make List<SimpleLookup> based on past searches if available 
 // V1.2: 2024-01-13: add history drop down menu and cache results
-// V1.1: 2023-12-26: remove sketchy filtering of species names etc which is not useful when maginating
-// V1.0: 2022-12-30: inital port of 2021 python gtk ui
+// V1.1: 2023-12-26: remove sketchy filtering of species names etc which is not useful
+// V1.0: 2022-12-30: initial port of 2021 python gtk ui
 
 public class App
 {
@@ -15,7 +16,7 @@ public class App
 	public static void main(String[] args)
 	{
 		System.setProperty("apple.laf.useScreenMenuBar", "true");
-		Debug.logTimestamp("Starting V1.2");
+		Debug.logTimestamp("Starting V1.3");
 		UiConstants.initFlags();
 		for(final String arg : args)
 		{
