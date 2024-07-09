1dd4d45d1a7cbbbfd17d4eb587c116c15eecd623
Daniel <danieltjandra@gmail.com>
2024-07-08 22:05:08 -0700

    Description

--- src/dt/jdictionary/App.java
+++ src/dt/jdictionary/App.java
@@ -4,7 +4,7 @@ import dt.jdictionary.ui.UiConstants;
 import dt.jdictionary.ui.UiMain;
 import dt.jdictionary.util.Debug;
 
-// V1.3: 2024-07-08: make List<SimpleLookup> based on past searches if available 
+// V1.3: 2024-07-08: make List<SimpleLookup> rank based on past searches if available 
 // V1.2: 2024-01-13: add history drop down menu and cache results
 // V1.1: 2023-12-26: remove sketchy filtering of species names etc which is not useful
 // V1.0: 2022-12-30: initial port of 2021 python gtk ui
