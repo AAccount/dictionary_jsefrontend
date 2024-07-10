1f486ea5a4de0a1a2a9ccc474c14f32e05c23b60
Daniel <danieltjandra@gmail.com>
2024-07-09 21:46:41 -0700

    Cleanup

--- src/dt/jdictionary/sqlite/load/WordBlob.java
+++ src/dt/jdictionary/sqlite/load/WordBlob.java
@@ -7,9 +7,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import dt.jdictionary.events.EventUtils;
 import dt.jdictionary.util.ChineseText;
